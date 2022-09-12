eeec20adbaadf04b0be94f9daf6c3549240c2f10
hayapo <keep.forward1001@gmail.com>
2022-09-12 21:40:05 +0900

    :art: 記事本文の入稿方法をテキストエリアにしたのでその対応
    markdown -> html -> Reactの順番で変換している

---
+++ src/lib/markdownToHtml.ts
@@ -0,0 +1,15 @@
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
+
+export const markdownToHtml = async (markdown: string) => {
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process(markdown);
+  return result.toString();
+};
